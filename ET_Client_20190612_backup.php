d291c6c5a8d2456887616535401a33e79545316c
koge <koge.k@dentsudigital.co.jp>
2019-06-12 12:10:59 +0900

    no message

--- ET_Client_20190612_backup.php
+++ ET_Client_20190612_backup.php
@@ -85,24 +85,16 @@ class ET_Client extends SoapClient {
 
 			if (is_null($this->getAuthToken($this->tenantKey)) || ($timeDiff < 5) || $forceRefresh  ){
 				$url = $this->tenantKey == null 
-						? "https://login.salesforce.com/services/oauth2/token"
+						? "https://auth.exacttargetapis.com/v1/requestToken?legacy=1"
 						: "https://www.exacttargetapis.com/provisioning/v1/tenants/{$this->tenantKey}/requestToken?legacy=1";
 				$jsonRequest = new stdClass(); 
-				$jsonRequest->grant_type    = "password";
-				$jsonRequest->client_id     = $this->clientId;
-				$jsonRequest->client_secret = $this->clientSecret;
-				$jsonRequest->username      = 'koge.k@dentsudigital.co.jp';
-				$jsonRequest->password      = '@kkkk3690';
-
-
-
-$post = json_encode($jsonRequest);
-var_dump($post);
-
-
-				$authResponse = restPost($url, $post);
-var_dump(999);
-
+				$jsonRequest->clientId = $this->clientId;
+				$jsonRequest->clientSecret = $this->clientSecret;
+				$jsonRequest->accessType = "offline";
+				if (!is_null($this->getRefreshToken($this->tenantKey))){
+					$jsonRequest->refreshToken = $this->getRefreshToken($this->tenantKey);
+				}
+				$authResponse = restPost($url, json_encode($jsonRequest));
 				$authObject = json_decode($authResponse->body);
 				
 				if ($authResponse && property_exists($authObject,"accessToken")){		
@@ -1736,14 +1728,14 @@ function restGet($url) {
 	*
  * @return string     The response payload from the REST service
  */
-function restPost($url, $content, $access_token = '') {
+function restPost($url, $content) {
 	$ch = curl_init();
 	
 	// Uses the URL passed in that is specific to the API used
 	curl_setopt($ch, CURLOPT_URL, $url);	
 	
 	// When posting to a Fuel API, content-type has to be explicitly set to application/json
-	$headers = array("Content-Type: application/json", "Authorization: OAuth ${access_token}");
+	$headers = array("Content-Type: application/json", "User-Agent: ".getSDKVersion());
 	curl_setopt ($ch, CURLOPT_HTTPHEADER, $headers);
 	
 	// The content is the JSON payload that defines the request
